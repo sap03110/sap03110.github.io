1ffb5191e6b6f00a541dbacbeae0b94afae2d6ba
guiyomj <wastfg6972@naver.com>
2022-04-02 23:15:18 +0900

    refactor: firebase 연동 및 이미지, 프로젝트 데이터 백엔드에서 받아오도록 수정
    기존 프로젝트 내 정적 이미지, json 파일을 읽어서 렌더링하던 방식을 firebase에서 가져오도록 수정
    스크롤 함수 리팩토링

---
+++ assets/constants/urls.js
@@ -0,0 +1,2 @@
+export const BACKEND_URL = 'https://portfolio-fb3e3-default-rtdb.asia-southeast1.firebasedatabase.app/projects.json';
+export const PHOTO_URL = 'https://firebasestorage.googleapis.com/v0/b/portfolio-fb3e3.appspot.com/o';
